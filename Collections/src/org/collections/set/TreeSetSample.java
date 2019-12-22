c24809efb2edc05f8aaf62dcaf37ba0e3a28ce99
Subhan383 <abdulsubhan0383@gmail.com>
2019-12-22 13:44:45 +0530

    Modified TreeSet

--- Collections/src/org/collections/set/TreeSetSample.java
+++ Collections/src/org/collections/set/TreeSetSample.java
@@ -3,31 +3,23 @@ package org.collections.set;
 import java.util.Set;
 import java.util.TreeSet;
 
-import org.collections.model.Student;
+
 
 public class TreeSetSample {
 
 	public static void main(String[] args) {
 	
-		Student std1=new Student();
-		std1.setClassName("10th Class");
-		std1.setMarks(383);
-		std1.setRollNumber(518);
 		
-		Set<Student> setoftree=new TreeSet<>();
-		setoftree.add(std1);
 		
-		Student std2=new Student();
-		std2.setClassName("Inter");
-		std2.setMarks(896);
-		std2.setRollNumber(562);
+		Set<String> setoftree=new TreeSet<>();
+		setoftree.add("abc");
+		setoftree.add("bcd");
+		setoftree.add("abc");
 		
-		setoftree.add(std2);
-		
-		for(Student tmp: setoftree) {
-			System.out.println("class is:"+tmp.getClassName()+" "+"Scored Marks are:"+tmp.getMarks()+" "+"RollNumber Is:"+tmp.getRollNumber());
-			
+		for(String tmp: setoftree) {
+			System.out.println(tmp);
 		}
+		
 		
 
 	}
