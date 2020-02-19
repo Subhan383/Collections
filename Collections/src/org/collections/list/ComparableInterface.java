5bba7b5e7811e7d9786faaad79675a064c5a353d
Subhan383 <abdulsubhan0383@gmail.com>
2020-02-19 11:23:12 +0530

    Added comparable example

---
+++ Collections/src/org/collections/list/ComparableInterface.java
@@ -0,0 +1,60 @@
+package org.collections.list;
+
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.ListIterator;
+
+public class ComparableInterface {
+
+	class Names implements Comparable<Names> {
+		private String name;
+
+		public Names(String name) {
+			super();
+			this.name = name;
+		}
+
+		@Override
+		public int compareTo(Names obj) {
+			if (name.length() == obj.name.length()) {
+				return 0;
+			} else if (name.length() < obj.name.length()) {
+				return -1;
+			} else
+				return 1;
+		}
+
+		@Override
+		public String toString() {
+			return name;
+		}
+
+	}
+
+	public static void main(String[] args) {
+		List names=new LinkedList<Names>();
+		names.add("Subhan");
+		names.add("Gopi");
+		names.add("Sriram");
+		names.add("Imran");
+		names.add("Vali");
+		names.add("Siva");
+		
+		ComparableInterface example = new ComparableInterface();
+        example.displayNames(names);
+        Collections.sort(names);
+        System.out.println("*************");
+        example.displayNames(names);
+	}
+
+	public void displayNames(List names) {
+		ListIterator<Names> iterator = names.listIterator();
+		while(iterator.hasNext()) {
+			System.out.println("Your name is: "+iterator.next());
+		}
+	}
+	
+	
+	
+}
